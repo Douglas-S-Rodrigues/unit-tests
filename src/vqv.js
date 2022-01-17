6c5c700f564fc2fc381244c49d4b0f7d3b54ffdc
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-01-17 19:29:21 -0300

    test vqv retornando string

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,11 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (nome, idade) => {
+  if (nome !== undefined || idade !== undefined) {
+   return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  }
+  return undefined;
+};
 
 module.exports = vqv;
