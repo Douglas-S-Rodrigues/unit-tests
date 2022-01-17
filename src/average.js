73f45dabc37c3e3f16ab7a68835cf5830100bcb6
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-01-17 17:28:26 -0300

    test function average concluido

--- src/average.js
+++ src/average.js
@@ -12,8 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let media = 0;
+  let soma = 0;
+  // se o array for vazio
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    soma += array[index];
+    media = Math.round(soma / array.length);
+  }
+  return media;
 };
 
 module.exports = average;
