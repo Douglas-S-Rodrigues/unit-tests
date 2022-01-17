c3978bbd4a7f269a1f6d86b3cf1a32ffb938a75a
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-01-17 19:39:21 -0300

    resolvendo problema com o  lint no test vqv

--- src/vqv.js
+++ src/vqv.js
@@ -18,7 +18,8 @@
 
 const vqv = (nome, idade) => {
   if (nome !== undefined || idade !== undefined) {
-   return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+   return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,
+   \ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
   }
   return undefined;
 };
