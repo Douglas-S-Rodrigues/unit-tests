49673d013c7c30924b2d517370a26d8910ad2d82
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-01-20 14:35:31 -0300

    funcao para os testes createStudent

--- src/testesDefuncoes.js
+++ src/testesDefuncoes.js
@@ -1,6 +1,6 @@
-const radius = 3;
-const circumference = 3;
-const pi = 3.14;
-const area = pi * radius * radius;
+// const radius = 3;
+// const circumference = 3;
+// const pi = 3.14;
+// const area = pi * radius * radius;
 
-console.log(area);
+// console.log(area);
